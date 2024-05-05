035fc050c7c587fd1e03a11d703b2a629e82da77
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-05 18:08:35 +0300

    Fix locator for login_page test

--- qa_guru_diplom_ui/pages/login_page.py
+++ qa_guru_diplom_ui/pages/login_page.py
@@ -3,7 +3,7 @@ from selene import browser, have
 
 class LoginPage:
     def click_on_login(self):
-        browser.all('[role="navigation"]').element_by(have.text('войти')).click()
+        browser.all('.global_action_link').element_by(have.text('войти')).click()
 
     def check_login_page(self):
         browser.element('button[type="submit"]').should(have.exact_text('Войти'))
