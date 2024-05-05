a1b2c314fc968ad9277360fdce7a2e8f9b7d3c2d
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-05 17:55:25 +0300

    Move tests for cart to test_cart.py

---
+++ tests/test_cart.py
@@ -0,0 +1,46 @@
+from qa_guru_diplom_ui.pages.main_page import main_page
+from qa_guru_diplom_ui.pages.search_page import search_page
+from qa_guru_diplom_ui.pages.cart_page import cart_page
+
+
+def test_add_game_to_cart():
+    main_page.open_main_page()
+
+    search_page.click_on_search()
+    search_page.find_game_in_search(game_name="Baldur's Gate 3")
+    search_page.click_on_first_game_in_search_row()
+    cart_page.add_game_to_cart()
+    cart_page.move_to_cart()
+
+    cart_page.check_game_in_cart(game_name="Baldur's Gate 3")
+
+
+def test_remove_game_from_cart():
+    main_page.open_main_page()
+
+    search_page.click_on_search()
+    search_page.find_game_in_search(game_name="Baldur's Gate 3")
+    search_page.click_on_first_game_in_search_row()
+    cart_page.add_game_to_cart()
+    cart_page.move_to_cart()
+    cart_page.remove_game_from_cart()
+
+    cart_page.check_empty_cart()
+
+
+def test_clear_cart():
+    main_page.open_main_page()
+
+    search_page.click_on_search()
+    search_page.find_game_in_search(game_name="Baldur's Gate 3")
+    search_page.click_on_first_game_in_search_row()
+    cart_page.add_game_to_cart()
+    cart_page.continue_buying()
+    search_page.click_on_search()
+    search_page.find_game_in_search(game_name="GreedFall")
+    search_page.click_on_first_game_in_search_row()
+    cart_page.add_game_to_cart()
+    cart_page.move_to_cart()
+    cart_page.clear_cart()
+
+    cart_page.check_empty_cart()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,5 +1,4 @@
 from qa_guru_diplom_ui.pages.main_page import main_page
-from qa_guru_diplom_ui.pages.search_page import search_page
 
 
 def test_switch_tab():
@@ -17,41 +16,3 @@ def test_change_language():
     main_page.choose_lang(lang="french")
 
     main_page.check_french_lang_on_page()
-
-
-def test_add_game_to_cart():
-    main_page.open_main_page()
-
-    search_page.click_on_search()
-    search_page.find_game_in_search(game_name="Baldur's Gate 3")
-    search_page.click_on_first_game_in_search_row()
-    main_page.add_game_to_cart()
-    main_page.open_cart()
-
-    main_page.check_game_in_cart()
-
-
-def test_remove_game_from_cart():
-    main_page.open_main_page()
-
-    search_page.click_on_search()
-    search_page.find_game_in_search(game_name="Baldur's Gate 3")
-    search_page.click_on_first_game_in_search_row()
-    main_page.add_game_to_cart()
-    main_page.open_cart()
-    main_page.remove_game_from_cart()
-
-    main_page.check_empty_cart()
-
-
-def test_clear_cart():
-    main_page.open_main_page()
-
-    search_page.click_on_search()
-    search_page.find_game_in_search(game_name="Baldur's Gate 3")
-    search_page.click_on_first_game_in_search_row()
-    main_page.add_game_to_cart()
-    main_page.open_cart()
-
-    main_page.clear_cart()
-    main_page.check_empty_cart()
