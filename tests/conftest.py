84884f94e975938c761da157a7780ac4af4e6bf0
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-07 20:55:18 +0300

    Add env file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selene import browser
 
-
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     options = Options()
@@ -16,8 +21,11 @@ def browser_management():
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASS')
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
         options=options
     )
 
