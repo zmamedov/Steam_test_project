bcc0744c562002e7a5bfa10bedb79b5ddf451a4f
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-07 20:30:32 +0300

    Add selenoid options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from selene import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://store.steampowered.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
