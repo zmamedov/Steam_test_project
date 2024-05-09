1ee394056fe7877b7c2f5a05260759806659be1f
Zaur Mamedov <zaurmd1996@gmail.com>
2024-05-09 12:45:54 +0300

    Add attachments of screenshot, html page, logs and video

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selene import browser
 
+from steam_test_project.utils import attach
+
+
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
+
+
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     options = Options()
@@ -35,5 +40,10 @@ def browser_management():
     browser.config.window_width = 1920
 
     yield
+
+    attach.add_screenshot(browser)
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
 
     browser.quit()
